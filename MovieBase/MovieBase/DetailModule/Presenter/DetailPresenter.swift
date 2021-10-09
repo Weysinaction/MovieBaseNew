3065cca9ef4ef2078a4ec9ce23e33b69b1ff83ee
[Weysinaction] <[lipatoff.const@gmail.com]>
2021-10-09 18:41:23 +0300

    Добавил роутер и ассамблею, а также покрыл юнит тестами.

--- MovieBase/MovieBase/DetailModule/Presenter/DetailPresenter.swift
+++ MovieBase/MovieBase/DetailModule/Presenter/DetailPresenter.swift
@@ -10,6 +10,7 @@ protocol DetailViewProtocol: AnyObject {
 protocol DetailViewPresenterProtocol: AnyObject {
     var film: Film? { get set }
     var networkService: ImageNetworkServiceProtocol! { get set }
+    func tap()
 }
 
 /// DetailViewPresenter-
@@ -17,14 +18,20 @@ final class DetailPresenter: DetailViewPresenterProtocol {
     // MARK: public properties
 
     var film: Film?
-    private weak var view: DetailViewProtocol!
+    private weak var view: DetailViewProtocol?
     internal var networkService: ImageNetworkServiceProtocol!
+    private var router: RouterProtocol?
 
     // MARK: init
 
-    init(view: DetailViewProtocol, film: Film, networkService: ImageNetworkServiceProtocol) {
+    init(view: DetailViewProtocol, film: Film, networkService: ImageNetworkServiceProtocol, router: RouterProtocol) {
         self.view = view
         self.film = film
         self.networkService = networkService
+        self.router = router
+    }
+
+    func tap() {
+        router?.popToRoot()
     }
 }
---
+++ MovieBase/MovieBase/Router.swift
@@ -0,0 +1,49 @@
+// Router.swift
+// Copyright © RoadMap. All rights reserved.
+
+import Foundation
+import UIKit
+
+protocol RouterMain {
+    var navigationController: UINavigationController? { get set }
+    var assemblyBuilder: AssemblyBuilderProtocol? { get set }
+}
+
+protocol RouterProtocol: RouterMain {
+    func initialViewController()
+    func showDetail(film: Film?)
+    func popToRoot()
+}
+
+/// Router-
+class Router: RouterProtocol {
+    var navigationController: UINavigationController?
+
+    var assemblyBuilder: AssemblyBuilderProtocol?
+
+    init(navigationController: UINavigationController, assemblyBuilder: AssemblyBuilderProtocol) {
+        self.navigationController = navigationController
+        self.assemblyBuilder = assemblyBuilder
+    }
+
+    func initialViewController() {
+        if let navigationController = navigationController {
+            guard let mainViewController = assemblyBuilder?.createMain(router: self) else { return }
+            navigationController.viewControllers = [mainViewController]
+        }
+    }
+
+    func showDetail(film: Film?) {
+        if let navigationController = navigationController {
+            guard let film = film,
+                  let detailViewController = assemblyBuilder?.createDetail(router: self, film: film) else { return }
+            navigationController.pushViewController(detailViewController, animated: true)
+        }
+    }
+
+    func popToRoot() {
+        if let navigationController = navigationController {
+            navigationController.popToRootViewController(animated: true)
+        }
+    }
+}
